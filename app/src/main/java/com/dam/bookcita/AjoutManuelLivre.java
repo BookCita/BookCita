d3400e60a94ac7558e44274bf0e17d897b1b930e
Sonia Berriri <berririsonia369@gmail.com>
2022-06-23 13:42:23 +0200

    Test

---
+++ app/src/main/java/com/dam/bookcita/AjoutManuelLivre.java
@@ -0,0 +1,14 @@
+package com.dam.bookcita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjoutManuelLivre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_manuel_livre);
+    }
+}
